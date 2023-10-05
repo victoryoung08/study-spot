5a4ad101a457d31d4a65ce03686fb559e722ea6b
jomarmontuya <jomar.montuya@outlook.com>
2023-10-05 22:17:27 +0800

    updates script

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -108,6 +108,8 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+
+          <button className="btn bg-red-500">Test again</button>
         </a>
       </div>
     </main>
