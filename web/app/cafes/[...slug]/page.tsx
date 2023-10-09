6e5fd3e972856a7b77d99752cc5e4a78cefd594f
jomarmontuya <jomar.montuya@outlook.com>
2023-10-10 02:26:50 +0800

    cleanup

--- web/app/cafes/[...slug]/page.tsx
+++ web/app/cafes/[...slug]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Container } from "../../components/common/Container";
 import * as component from "../../components/common/ComponentSelector";
 
