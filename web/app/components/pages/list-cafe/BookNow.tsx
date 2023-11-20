cba1609f30a9671fbfedbb2af92df1539b4d5d1d
jomarmontuya <jomar.montuya@outlook.com>
2023-11-20 13:45:49 +0800

    add redirect to stripe

--- web/app/components/pages/list-cafe/BookNow.tsx
+++ web/app/components/pages/list-cafe/BookNow.tsx
@@ -5,7 +5,7 @@ import Markdown from "markdown-to-jsx";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Lottie from "lottie-react";
 import { PaperPlane } from "@/animations/paperplane";
 
@@ -74,6 +74,14 @@ export default function BookNow({
     }
   };
 
+  useEffect(() => {
+    if (formSubmitted && !error) {
+      setTimeout(() => {
+        window.location.href = book_in_a_call_link || "/";
+      }, 500);
+    }
+  });
+
   return (
     <Container>
       <div className="text-center lg:py-20" id="book-now">
@@ -108,109 +116,96 @@ export default function BookNow({
 
         <div className="mt-10">
           <div id="formContainer" className="mt-20">
-            {formSubmitted && !error ? (
-              <div className="flex justify-center items-center flex-col">
-                <Lottie
-                  style={{ width: 300, textAlign: "center" }}
-                  animationData={PaperPlane}
-                  loop
-                />
-                <p className="text-sm">
-                  Thank you for reaching out, your message is on its way.
-                </p>
-              </div>
-            ) : (
-              <div>
-                <form
-                  onSubmit={handleSubmit(onSubmit)}
-                  className="w-3/4 lg:w-2/5 mx-auto flex flex-col gap-4"
-                >
-                  <div>
-                    {errors.first_name && (
-                      <div className="flex justify-start ml-4 mb-1">
-                        <span className="text-xs text-red-400">
-                          {errors.first_name.message}
-                        </span>
-                      </div>
-                    )}
-                    <input
-                      type="text"
-                      {...register("first_name")}
-                      placeholder="Your First Name"
-                      className="input input-bordered w-full rounded-2xl border-white text-sm bg-[#3a3939] focus:border-primary"
-                    />
-                  </div>
-
-                  <div>
-                    {errors.cafe_name && (
-                      <div className="flex justify-start ml-4 mb-1">
-                        <span className="text-xs text-red-400">
-                          {errors.cafe_name.message}
-                        </span>
-                      </div>
-                    )}
-                    <input
-                      type="text"
-                      {...register("cafe_name")}
-                      placeholder="Your Cafe Name"
-                      className="input input-bordered w-full rounded-2xl border-white text-sm bg-[#3a3939] focus:border-primary"
-                    />
-                  </div>
-
-                  <div>
-                    {errors.email && (
-                      <div className="flex justify-start ml-4 mb-1">
-                        <span className="text-xs text-red-400">
-                          {errors.email.message}
-                        </span>
-                      </div>
-                    )}
-                    <input
-                      type="text"
-                      {...register("email")}
-                      placeholder="Your best email"
-                      className="input input-bordered w-full rounded-2xl border-white text-sm bg-[#3a3939] focus:border-primary"
-                    />
-                  </div>
-
-                  <div>
-                    {errors.phone && (
-                      <div className="flex justify-start ml-4 mb-1">
-                        <span className="text-xs text-red-400">
-                          {errors.phone.message}
-                        </span>
-                      </div>
-                    )}
-                    <input
-                      type="text"
-                      {...register("phone")}
-                      placeholder="Your best contact number"
-                      className="input input-bordered w-full  rounded-2xl border-white text-sm bg-[#3a3939] focus:border-primary"
-                    />
-                  </div>
-                  {error && (
-                    <p className="text-sm text-warning">
-                      Error Submitting Form, Please try again
-                    </p>
+            <div>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="w-3/4 lg:w-2/5 mx-auto flex flex-col gap-4"
+              >
+                <div>
+                  {errors.first_name && (
+                    <div className="flex justify-start ml-4 mb-1">
+                      <span className="text-xs text-red-400">
+                        {errors.first_name.message}
+                      </span>
+                    </div>
                   )}
-                  <button
-                    type="submit"
-                    className="mt-10 btn btn-primary capitalize font-normal border-white hover:border-white"
+                  <input
+                    type="text"
+                    {...register("first_name")}
+                    placeholder="Your First Name"
+                    className="input input-bordered w-full rounded-2xl border-white text-sm bg-[#3a3939] focus:border-primary"
+                  />
+                </div>
+
+                <div>
+                  {errors.cafe_name && (
+                    <div className="flex justify-start ml-4 mb-1">
+                      <span className="text-xs text-red-400">
+                        {errors.cafe_name.message}
+                      </span>
+                    </div>
+                  )}
+                  <input
+                    type="text"
+                    {...register("cafe_name")}
+                    placeholder="Your Cafe Name"
+                    className="input input-bordered w-full rounded-2xl border-white text-sm bg-[#3a3939] focus:border-primary"
+                  />
+                </div>
+
+                <div>
+                  {errors.email && (
+                    <div className="flex justify-start ml-4 mb-1">
+                      <span className="text-xs text-red-400">
+                        {errors.email.message}
+                      </span>
+                    </div>
+                  )}
+                  <input
+                    type="text"
+                    {...register("email")}
+                    placeholder="Your best email"
+                    className="input input-bordered w-full rounded-2xl border-white text-sm bg-[#3a3939] focus:border-primary"
+                  />
+                </div>
+
+                <div>
+                  {errors.phone && (
+                    <div className="flex justify-start ml-4 mb-1">
+                      <span className="text-xs text-red-400">
+                        {errors.phone.message}
+                      </span>
+                    </div>
+                  )}
+                  <input
+                    type="text"
+                    {...register("phone")}
+                    placeholder="Your best contact number"
+                    className="input input-bordered w-full  rounded-2xl border-white text-sm bg-[#3a3939] focus:border-primary"
+                  />
+                </div>
+                {error && (
+                  <p className="text-sm text-warning">
+                    Error Submitting Form, Please try again
+                  </p>
+                )}
+                <button
+                  type="submit"
+                  className="mt-10 btn btn-primary capitalize font-normal border-white hover:border-white"
+                >
+                  {loading ? "Submitting..." : "Get Started"}
+                </button>
+                <div className="mt-5">
+                  <Link
+                    target="_blank"
+                    href={book_in_a_call_link || "/"}
+                    className="text-base underline"
                   >
-                    {loading ? "Submitting..." : "Get Started"}
-                  </button>
-                  <div className="mt-5">
-                    <Link
-                      target="_blank"
-                      href={book_in_a_call_link || "/"}
-                      className="text-base underline"
-                    >
-                      Book in a call first
-                    </Link>
-                  </div>
-                </form>
-              </div>
-            )}
+                    Book in a call first
+                  </Link>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
