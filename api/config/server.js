22cb4edbbb8d90a9d913146637c028efab6a3810
montuyamaricela <montuyamaricela@gmail.com>
2024-01-05 22:55:42 +0800

    fetch sitemap from api

--- api/config/server.js
+++ api/config/server.js
@@ -1,10 +1,14 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
+  // Uncomment this section only when you have thoroughly reviewed and tested it
+  // url: {
+  //   sitemap: "https://studyspotcafe.com/sitemap/index.xml",
+  // },
 });
