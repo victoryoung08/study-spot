839c0081f5cd34e20d73f0a4e9c291db5add6218
montuyamaricela <montuyamaricela@gmail.com>
2024-02-04 03:22:31 +0800

    fix typing errors

--- web/src/queries/getCafeDetails.tsx
+++ web/src/queries/getCafeDetails.tsx
@@ -4,10 +4,14 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
 import fetchWrapper from "../helper/fetchWrapper";
 
+interface UserData {
+  // Define the structure of your user data here
+  cafe?: any; // Replace 'any' with the actual type of 'cafe'
+}
+
 export default async function getCafeDetails() {
   const session = await getServerSession(authOptions);
   const { access } = session?.user || {};
-  //   console.log("access", access);
   try {
     if (!access) {
       throw new Error("Invalid token data");
@@ -23,11 +27,14 @@ export default async function getCafeDetails() {
       },
     });
 
-    if (data) {
-      // If data is an array, use data[0]?.cafe; otherwise, use data?.cafe directly
-      const cafeDetails = Array.isArray(data) ? data[0]?.cafe : data.cafe;
+    const cafeData = data as UserData;
 
-      console.log("cafeDetails:", cafeDetails);
+    if (cafeData) {
+      // If data is an array, use data[0]?.cafe; otherwise, use data?.cafe directly
+      const cafeDetails = Array.isArray(cafeData)
+        ? cafeData[0]?.cafe
+        : cafeData?.cafe;
+
       return { cafeDetails };
     }
   } catch (error) {
