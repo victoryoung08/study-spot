1db02909c8240cf92d915ccc5a85dd24b3322b1d
montuyamaricela <montuyamaricela@gmail.com>
2024-02-19 00:31:58 +0800

    fix ga api

--- web/src/pages/api/ga.ts
+++ web/src/pages/api/ga.ts
@@ -69,7 +69,7 @@ export default async function handler(
   } catch (error) {
     console.error("Error fetching analytics data:", error);
     return res.status(500).json({
-      error: "Internal Server Error",
+      error: error,
     });
   }
 }
